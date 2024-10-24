a17a4fe9f01bba921e9c50b948f6fc03b7563c95
TheKey-Webworks <163067791+TheKey-Webworks@users.noreply.github.com>
2024-10-24 15:24:40 -0300

    Create microfarm_gohan.lua

---
+++ microfarm_gohan.lua
@@ -0,0 +1,102 @@
+print("V1.0")
+
+local Player = game:GetService("Players").LocalPlayer
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+
+-- id del usuario
+local userId = Player.UserId
+
+--datos del usuario
+local playerData = ReplicatedStorage.Datas[userId]
+
+--datos de las misiones
+local activeQuest = playerData.Quest
+local questProgress = playerData.QuestProgress
+
+--eventos
+local teleportEvent = ReplicatedStorage.Package.Events.TP
+local getQuestEvent = ReplicatedStorage.Package.Events.Qaction
+local punchEvent = ReplicatedStorage.Package.Events.p
+local rebEvent = ReplicatedStorage.Package.Events.reb
+local chargeEvent = ReplicatedStorage.Package.Events.cha
+local mel = ReplicatedStorage.Package.Events.mel
+
+-- función para obtener el character
+local function getCharacter () 
+    local c = Player.Character or Player.CharacterAdded:Wait()
+    return c
+end
+
+-- función para tomar una quest
+local function getQuest(questName)
+    local questGiver = workspace.Others.NPCs[questName]
+    local c = getCharacter()
+    c.HumanoidRootPart.CFrame = questGiver.HumanoidRootPart.CFrame
+    task.wait(2)
+    getQuestEvent:InvokeServer(questGiver)
+end
+
+-- función para matar a gohan
+local function farmNPC(npc)
+    local gohan = workspace.Living:WaitForChild(npc)
+    if not gohan or not gohan:FindFirstChild("Humanoid") then
+        return 
+    end
+
+    if gohan.Humanoid.Health > 0 then
+        while _G.farm and gohan.Humanoid.Health > 0 and task.wait() do
+            
+            -- mover el personaje hasta gohan
+            coroutine.wrap(function()
+                -- character del player
+                local c = getCharacter()
+                -- HumanoidRootPart del player
+                local cHRP = c:FindFirstChild("HumanoidRootPart") or c:WaitForChild("HumanoidRootPart")
+                -- HumanoidRootPart de gohan 
+                local eHRP = gohan:FindFirstChild("HumanoidRootPart") or gohan:WaitForChild("HumanoidRootPart")
+
+                if not eHRP then return end
+
+                -- actualizar la posicion del personaje
+                while _G.farm and gohan.Humanoid.Health > 0 and task.wait() do
+                    if not eHRP then break end
+                    if not cHRP then cHRP = c:FindFirstChild("HumanoidRootPart") or c:WaitForChild("HumanoidRootPart") end
+                    cHRP.CFrame = eHRP.CFrame
+                    task.wait()
+                end
+            end)()
+
+            -- atacar
+            for i = 1, 4 do
+                punchEvent:FireServer("Blacknwhite27", i)
+            end
+
+            task.wait()
+        end
+    end
+end
+
+_G.farm = true
+_G.rebirth = true
+
+-- main loop
+while _G.farm and task.wait() do
+
+    if _G.rebirth then
+        rebEvent:InvokeServer()
+    end
+
+    local currentTime = game.Lighting.ClockTime
+    if currentTime > 0 and currentTime < 12 then 
+        teleportEvent:InvokeServer("Earth")
+    end
+
+    if activeQuest.Value == "" then
+        getQuest("Kid Nohag")
+    end
+
+    if activeQuest.Value ~= "" then
+        farmNPC("Kid Nohag")
+    end
+end
